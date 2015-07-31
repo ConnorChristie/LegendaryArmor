27cf09ef42bb5aa66298c6c3565621a675d33344
Ethan <ethanzeigler@gmail.com>
2015-07-30 20:58:31 -0400

    Added redeem, fixed bug where players could give themselves legedaries. May be errors based on my guessed getters.

--- src/com/enjin/devection/commands/LegendaryCommands.java
+++ src/com/enjin/devection/commands/LegendaryCommands.java
@@ -1,5 +1,6 @@
 package com.enjin.devection.commands;
 
+import com.enjin.devection.main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
@@ -8,40 +9,63 @@ import org.bukkit.command.CommandSender;
 
 import com.enjin.devection.armor.LegendaryArmor;
 import com.enjin.devection.util.Utils;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class LegendaryCommands implements CommandExecutor
 {
+    Main main;
 	private String PREFIX = "[LegendaryArmor] ";
-	
-	@SuppressWarnings("deprecation")
+
+    public LegendaryCommands(Main main) {
+        this.main = main;
+    }
+
+    @SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
-		if (label.equalsIgnoreCase("legend"))
+		if (cmd.getName().equalsIgnoreCase("legend"))
 		{
-			if (args.length == 2)
-			{
-				OfflinePlayer player = Bukkit.getOfflinePlayer(args[0]);
-				LegendaryArmor armor = LegendaryArmor.armor(args[1]);
-				
-				if (player != null && player.hasPlayedBefore())
-				{
-					if (armor != null)
-					{
-						if (player.isOnline())
-						{
-							
-						} else
-						{
-							Utils.addToOfflineInventory(player, armor.getItem());
-						}
-					} else
-						sender.sendMessage(PREFIX + "Could not find specified armor");
-				} else
-					sender.sendMessage(PREFIX + "Could not find specified player");
-			} else
-				sender.sendMessage(PREFIX + "Usage: /legend <player> <legendary>");
+            if(sender instanceof ConsoleCommandSender) {
+                if (args.length == 2) {
+                    OfflinePlayer player = Bukkit.getOfflinePlayer(args[0]);
+                    LegendaryArmor armor = LegendaryArmor.armor(args[1]);
+
+                    if (player != null && player.hasPlayedBefore()) {
+                        if (armor != null) {
+                            if (player.isOnline()) {
+
+                            } else {
+                                Utils.addToOfflineInventory(player, armor.getItem());
+                            }
+                        } else
+                            sender.sendMessage(PREFIX + "Could not find specified armor");
+                    } else
+                        sender.sendMessage(PREFIX + "Could not find specified player");
+                } else sender.sendMessage(PREFIX + "Usage: /legend <player> <legendary> or /legend");
+            }
+            else if(sender instanceof Player)
+            {
+                if(args.length == 1)
+                {
+                    if(args[0].equalsIgnoreCase("redeem"))
+                    {
+                        if(main.getAwaitingLegendaries.contains(((Player) sender).getUniqueId()))
+                        {
+                            List<LegendaryArmor> toGive = main.getAwaitingLegendaries.get(((Player) sender).getUniqueId());
+                            for(LegendaryArmor armor: toGive)
+                            {
+                                ((Player) sender).getInventory().addItem(armor.getItem());
+                            }
+                            main.getAwaitingLegendaries.remove(((Player) sender).getUniqueId());
+                            sender.sendMessage(PREFIX + "You received your items.");
+                        } sender.sendMessage(PREFIX + "You have no redeemable legendary items.");
+                    } else sender.sendMessage(PREFIX + "Usage: /legend redeem");
+                }
+            }
 		}
-		
 		return true;
 	}
 }
